660368cbaca75c14f98c18ee2dba276a59e2f739
Adrian Chiriac <52508424+chiriacadrian@users.noreply.github.com>
2020-07-01 21:01:05 +0200

    Updates

---
+++ precache-manifest.b5cb9d0a35bd26ae870fefbb853e16f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f17bb2553459cd94823345842759742",
+    "url": "/expressionist/index.html"
+  },
+  {
+    "revision": "266bfa6b436eac3f96e6",
+    "url": "/expressionist/static/css/main.63a7557f.chunk.css"
+  },
+  {
+    "revision": "e7d6d5bc15a2a42fc03a",
+    "url": "/expressionist/static/js/2.197cad18.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expressionist/static/js/2.197cad18.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "266bfa6b436eac3f96e6",
+    "url": "/expressionist/static/js/main.fb0f12df.chunk.js"
+  },
+  {
+    "revision": "df9d204d847bf30ac484",
+    "url": "/expressionist/static/js/runtime-main.e6cfcaf7.js"
+  }
+]);
